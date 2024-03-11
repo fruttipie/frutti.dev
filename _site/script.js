1879b1fff7b64dca8743f35554b8f018e812398b
fruttipie80 <fruttipie@gmail.com>
2024-03-10 23:01:46 -0700

    Recovered the website
    - has header
    - had footer

---
+++ _site/script.js
@@ -0,0 +1,107 @@
+const themeToggle = document.getElementById("theme-toggle");
+const prefersDarkQuery = window.matchMedia("(prefers-color-scheme: dark)") //media query
+const moon = document.getElementById("moon");
+const sun = document.getElementById("sun");
+const itchio = document.getElementById("itchio");
+const welcomeMessages = [
+    "hello there!!",
+    "welcome!!",
+    "*:･ﾟ✧*:･ﾟ✧",
+    "yaaaaay!",
+    "stay a while and listen!",
+    "how's it hanging?",
+    "Isaac, we love you",
+    "land lubber ♡",
+    "what the fart?",
+    "✧polychrome✧",
+    "I've got a few tricks!",
+    "miku ♡",
+    "gumi ♡",
+    "miku miku oo ee uu",
+    "AA EE OO",
+    "Jerma985 here",
+    "a hemomancer",
+    "hmmmmmm",
+    "...",
+    "new wave of the future",
+    "!!!",
+    "!?",
+    "miku dayo",
+    "gamer moment",
+    "Play Insaniquarium",
+    "space cadet pinball",
+    "monday left me broken",
+    "like a G6",
+    //"what's ur mother's maiden name?",
+    //"what's these homies dissin my girl?",
+    "full of Red 40",
+    "i love mulch!",
+    "WARNING!!!",
+    "zot zot!!",
+    "hire me please!",
+    //"ooh you wanna hire me so bad",
+    "game designer",
+    "rogue-like fanatic",
+    "#b5ffcb",
+    "hello, world!",
+    "// fix this later",
+    "ouuughhhh",
+    //"why do they call it the restroom?",
+    "glorp",
+    "strange and weird",
+    "made with Jekyll & Sass",
+    "pad thai consumer",
+    "made with ♡",
+    "welcome to my sick site",
+    "<!-- What -->"
+];
+const CUSTOM_GREETING_ELEMENT_ID = "welcome";
+const customGreetingEl = document.getElementById(CUSTOM_GREETING_ELEMENT_ID);
+
+// For setting the theme to match the user's computer's theme.
+window.addEventListener("load", () => {
+    if (prefersDarkQuery.matches){
+        toggleMode();
+    } else {
+        setIcon("sun");
+    }
+});
+
+function setIcon(icon) { // for example: setIcon("moon") will set the icon TO the moon
+    if (icon === "moon"){
+        moon.classList.remove("hidden");
+        sun.classList.add("hidden");
+    } else {
+        moon.classList.add("hidden");
+        sun.classList.remove("hidden");
+    }
+}
+
+// function that changes the body from light to dark and visa versa:
+function toggleMode() {
+    document.body.classList.toggle("darkmode");
+    if (document.body.classList.contains("darkmode")){
+        setIcon("moon");
+    } else {
+        setIcon("sun");
+    }
+}
+
+// When the button is pressed, call the toggleMode function.
+themeToggle.addEventListener("click", toggleMode);
+
+// //if the user CHANGES their theme from dark mode into another mode (light), do this:
+// prefersDarkQuery.addEventListener("change", toggle);
+
+const updateInnerText = () => {
+    const randomGreetingIndex = Math.floor(Math.random() * welcomeMessages.length);
+    customGreetingEl.innerText = welcomeMessages[randomGreetingIndex];
+};
+  
+// addEventListener("load", updateInnerText);
+  
+customGreetingEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.info("Reloading");
+    updateInnerText();
+});
